df58b3774660743b9e9cb44f9910ec3d5255c3be
deltqz <78700987+deltqz@users.noreply.github.com>
2026-02-07 16:04:25 -0700

    Delete unnecesary files

--- build.bat
+++
@@ -1,6 +0,0 @@
-
-REM Q:\dev-project\libs\opencv\build
-
-
-cl /std:c++20 /I"Q:\dev-project\libs\opencv\build\include" ocr.cpp /link /LIBPATH:"Q:\dev-project\libs\opencv\build\x64\vc15\lib" opencv_world460.lib
-
